c40941f8174067addf4d4e8463926fcf05c78389
Driss El yaagoubi <del-yaag@e1r5p2.1337.ma>
2023-05-21 16:12:18 +0100

    add string functions

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: del-yaag <del-yaag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 21:45:16 by del-yaag          #+#    #+#             */
-/*   Updated: 2023/05/21 14:48:31 by del-yaag         ###   ########.fr       */
+/*   Updated: 2023/05/21 16:06:48 by del-yaag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,16 @@
 #include <signal.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+
+// string functions
+size_t	ft_strlen(const char *str);
+// -------------------------------
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+// split functions
+char	**ft_split(char const *s, char c);
+void	*ft_calloc(size_t number, size_t size);
 
 #endif
